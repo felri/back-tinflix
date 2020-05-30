ac7650eddc7b6db4e3c6abf0c72fb577f216f1c4
felri <felipercmelo@gmail.com>
2020-05-30 17:28:18 -0300

    env changes

--- db/db.js
+++ db/db.js
@@ -1,8 +1,8 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: process.env.USERNAME_DB,
-  host: 'localhost',
-  database: 'netflixtinder',
+  host: process.env.HOST,
+  database: process.env.DB,
   password: process.env.PASSWORD,
   port: 5432,
 })
